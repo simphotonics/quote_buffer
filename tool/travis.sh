f6cc810738fe0ecb351be979c4e68d2f83b07029
simphotonics <git@simphotonics.com>
2020-01-25 21:45:09 +0000

    Added call to dartfmt.

--- tool/travis.sh
+++ tool/travis.sh
@@ -7,7 +7,9 @@ BLUE='\033[1;34m'
 GREEN='\033[1;32m'
 CYAN='\033[1;36m'
 YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
 RESET='\033[0m'
+
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
@@ -22,6 +24,13 @@ pub get
 
 # Upgrade packages.
 pub upgrade
+
+echo
+echo -e "${PURPLE}=== Checking Source Code Formatting${RESET} $PWD..."
+echo
+# Overwrite files with formatted content: -w
+# Dry run: -n
+dartfmt -w $(find bin lib test -name \*.dart 2>/dev/null)
 
 # Analyze dart files
 echo
