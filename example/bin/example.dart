76fb7446cd16fe5e6d60cb60cb00b5e2a56d3723
simphotonics <git@simphotonics.com>
2020-07-15 18:06:32 +0100

    Amended usage.

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -4,7 +4,7 @@ import 'package:quote_buffer/quote_buffer.dart';
 // To run this program go to the folder 'quote_buffer/example'
 // in your terminal and type
 //
-// # dart example.dart
+// # dart bin/example.dart
 //
 // followed by enter.
 main(List<String> args) {
@@ -12,39 +12,38 @@ main(List<String> args) {
   strings.addAll(args);
 
   AnsiPen green = AnsiPen()..green(bold: true);
-  AnsiPen blue = AnsiPen()..blue(bold: true);
-  var buffer = QuoteBuffer();
+  final buffer = StringBuffer();
 
   // Adds:'\'1\'' (Note: Objects are first converted to strings.)
-  print(green('buffer.writeQ(29);'));
+  print(green('buffer.writeQ') + '(29);');
   buffer.writeQ(29);
   print(buffer.toString());
   buffer.clear();
 
   // Adds:'\'name\'\n'
-  print(green('buffer.writelnQ(\'name\');'));
-  buffer.writelnQ('name');
+  print(green('buffer.writelnQ') +
+      '(\'name\', delimiter: QuotationMark.DOUBLE);');
+  buffer.writelnQ('name', delimiter: QuotationMark.DOUBLE);
   print(buffer.toString());
   buffer.clear();
-
-  buffer = QuoteBuffer(
-    delimiter: QuotationMark.Double,
-  );
-  buffer.writeAllQ(strings, ',');
 
   // Prints:
   // "one,two,three,four"
-  print(blue('final buffer = QuoteBuffer('
-      'delimiter: QuotationMark.Double,'
-      '); '));
-
-  print(green('buffer.writeAllQ([\'one\',\'two\',\'three\',\'four\'],\',\');'));
+  buffer.writeAllQ(strings, separator: ',');
+  print(green('buffer.writeAllQ') +
+      '([\'one\',\'two\',\'three\',\'four\'], separator: \',\');');
   print(buffer.toString());
   buffer.clear();
 
-  print(green(
-    'buffer.writelnAllQ([\'one\',\'two\',\'three\',\'four\'], separator1: \'#\', separator2: \',\');',
-  ));
-  buffer.writelnAllQ(strings, separator1: '#', separator2: ',');
+  print(
+    green('buffer.writelnAllQ(') +
+        '[\'one\',\'two\',\'three\',\'four\'], separator1: \'#\', separator2: \',\');',
+  );
+  buffer.writelnAllQ(
+    strings,
+    separator1: '#',
+    separator2: ',',
+    delimiter: QuotationMark.DOUBLE,
+  );
   print(buffer.toString());
 }
