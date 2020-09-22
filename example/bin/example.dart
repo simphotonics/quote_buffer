65d33969ddb3a9b50cd338adb78f4e77a3d554f1
simphotonics <git@simphotonics.com>
2020-09-22 15:49:22 +0100

    Removed dep. on package ansicolor.

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,44 +1,56 @@
-import 'package:ansicolor/ansicolor.dart';
 import 'package:quote_buffer/quote_buffer.dart';
 
-// To run this program go to the folder 'quote_buffer/example'
-// in your terminal and type
-//
-// # dart bin/example.dart
-//
-// followed by enter.
-main(List<String> args) {
-  List<String> strings = ['one', 'two', 'three', 'four'];
+/// Ansi color modifier: Reset to default.
+const String RESET = '\u001B[0m';
+
+/// Ansi color modifier.
+const String BLUE = '\u001B[94m';
+
+/// Ansi color modifier.
+const String GREEN = '\u001B[1;32m';
+
+String blue(String input) => BLUE + input + RESET;
+String green(String input) => GREEN + input + RESET;
+
+/// To run this program in a terminal navigate to the
+/// root directory of the package `quote_buffer`and use:
+/// ```Console
+/// $ dart -enable-experiment=non-nullabe example/bin/example.dart
+/// ```
+/// followed by enter.
+void main(List<String> args) {
+  final strings = <String>['one', 'two', 'three', 'four'];
   strings.addAll(args);
 
-  AnsiPen green = AnsiPen()..green(bold: true);
   final buffer = StringBuffer();
 
+  print(green('QuoteBuffer Extension - Example'));
+
   // Adds:'\'1\'' (Note: Objects are first converted to strings.)
-  print(green('buffer.writeQ') + '(29);');
+  print(blue('buffer.writeQ(29);') + ' // Adding quotation marks.');
   buffer.writeQ(29);
   print(buffer.toString());
   buffer.clear();
 
   // Adds:'\'name\'\n'
-  print(green('buffer.writelnQ') +
-      '(\'name\', delimiter: QuotationMark.DOUBLE);');
+  print(blue('buffer.writelnQ'
+      '(\'name\', delimiter: QuotationMark.DOUBLE);') + ' // Adding double quotation marks and newline.');
   buffer.writelnQ('name', delimiter: QuotationMark.DOUBLE);
   print(buffer.toString());
   buffer.clear();
 
   // Prints:
   // "one,two,three,four"
-  buffer.writeAllQ(strings, separator: ',');
-  print(green('buffer.writeAllQ') +
-      '([\'one\',\'two\',\'three\',\'four\'], separator: \',\');');
+  buffer.writeAllQ(strings, separator: ', ');
+  print(blue('buffer.writeAllQ'
+      '([\'one\',\'two\',\'three\',\'four\'], separator: \', \');'));
   print(buffer.toString());
   buffer.clear();
 
-  print(
-    green('buffer.writelnAllQ(') +
-        '[\'one\',\'two\',\'three\',\'four\'], separator1: \'#\', separator2: \',\');',
-  );
+  print(blue(
+    'buffer.writelnAllQ('
+    '[\'one\',\'two\',\'three\',\'four\'], separator1: \'#\', separator2: \',\');',
+  ));
   buffer.writelnAllQ(
     strings,
     separator1: '#',
