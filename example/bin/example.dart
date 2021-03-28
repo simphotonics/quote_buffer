8f82392dd6e24d2f2b69e0bfdf13ecf3d0165d6f
simphotonics <git@simphotonics.com>
2021-03-28 21:40:12 +0100

    Using AnsiPen() to add color output.

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,56 +1,49 @@
 import 'package:quote_buffer/quote_buffer.dart';
-
-/// Ansi color modifier: Reset to default.
-const String RESET = '\u001B[0m';
-
-/// Ansi color modifier.
-const String BLUE = '\u001B[94m';
-
-/// Ansi color modifier.
-const String GREEN = '\u001B[1;32m';
-
-String blue(String input) => BLUE + input + RESET;
-String green(String input) => GREEN + input + RESET;
+import 'package:ansicolor/ansicolor.dart';
 
 /// To run this program in a terminal navigate to the
 /// root directory of the package `quote_buffer`and use:
 /// ```Console
-/// $ dart -enable-experiment=non-nullabe example/bin/example.dart
+/// $ dart example/bin/example.dart
 /// ```
 /// followed by enter.
 void main(List<String> args) {
-  final strings = <String>['one', 'two', 'three', 'four'];
+  final green = AnsiPen()..green(bold: true);
+  final blue = AnsiPen()..blue(bold: true);
+  final magenta = AnsiPen()..magenta(bold: true);
+
+  final strings = <String>['one', 'two', 'three'];
   strings.addAll(args);
 
   final buffer = StringBuffer();
 
-  print(green('QuoteBuffer Extension - Example'));
+  print(magenta('QuoteBuffer Extension - Example'));
 
   // Adds:'\'1\'' (Note: Objects are first converted to strings.)
-  print('// Adding quotation marks.');
+  print(green('// Adding quotation marks.'));
   print(blue('buffer.writeQ(29);'));
   buffer.writeQ(29);
   print(buffer.toString());
   buffer.clear();
 
   // Adds:'\'name\'\n'
-  print('// Adding double quotation marks and newline.');
+  print(green('// Adding double quotation marks and newline.'));
   print(blue('buffer.writelnQ'
       '(\'name\', delimiter: QuotationMark.DOUBLE);'));
-  buffer.writelnQ('name', delimiter: QuotationMark.DOUBLE);
+  buffer.writelnQ('name', delimiter: Punctuation.doubleQuotationMark);
   print(buffer.toString());
   buffer.clear();
 
   // Prints:
   // 'one', 'two', 'three', 'four'
-  print('// Adding separator and quotation marks.');
+  print(green('// Adding separator and quotation marks.'));
   buffer.writeAllQ(strings);
   print(blue('buffer.writeAllQ'
       '([\'one\',\'two\',\'three\',\'four\'], separator: \', \');'));
   print(buffer.toString());
   buffer.clear();
 
-  print('// Adding separator1, quotation marks, newline.');
+  print(green('// Adding separator1, quotation marks, separator2, newline.'));
   print(blue(
     'buffer.writelnAllQ('
     '[\'one\',\'two\',\'three\',\'four\'], separator1: \'#\', separator2: \',\');',
@@ -59,7 +52,7 @@ void main(List<String> args) {
     strings,
     separator1: '#',
     separator2: ',',
-    delimiter: QuotationMark.DOUBLE,
+    delimiter: Punctuation.doubleQuotationMark,
   );
   print(buffer.toString());
 }
